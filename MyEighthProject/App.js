ba3f375e530013021f3b77d00d69b2538b978b2e
LuisAceves <81989912+LuisAceves@users.noreply.github.com>
2021-05-10 04:26:10 +0000

    My Ninth Project
    Weather Forecast

--- MyEighthProject/App.js
+++ MyEighthProject/App.js
@@ -1,7 +1,51 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as sharing from 'expo-sharing';
 
 export default function App() {
+
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+    
+    if (permissionResult.granted === false){
+      alert('Permission is required!');
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri});
+
+  };
+
+  let openSharingDialogAsync = async () => {
+    if( !(await Sharing.isAvailableAsync())){
+      alert('Sharing is not available on my phone');
+      return;
+    }
+
+    Sharing.shareAsync(selectedImage.localUri);
+  };
+
+  if (selectedImage !== null) {
+    return(
+      <View style={styles.container}>
+        <Image source={{ uri: selectedImage.localUri}} style={styles.selImage}>
+
+          <TouchableOpacity> onPress={openSharingDialogAsync} style={styles.button}
+          <Text style={styles.buttonText}> Share This Photo </Text>
+          </TouchableOpacity>
+        </Image>
+      </View>
+    )
+  }
   
   return (
     <View style={styles.container}>
@@ -48,5 +92,11 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 20,
     color: '#ffff'
+  },
+
+  selImage: {
+    height: 300,
+    width: 300,
+    resizeMode: 'contain'
   }
 });
