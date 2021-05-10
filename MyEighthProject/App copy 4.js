35d90bad61a1e6954b317b7dc5554ea1b20fefb3
LuisAceves <81989912+LuisAceves@users.noreply.github.com>
2021-05-10 02:47:32 +0000

    Adding Buttons and Images

---
+++ MyEighthProject/App copy 4.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker'
+
+export default function App() {
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+    
+    if (permissionResult.granted === false){
+      alert('Permission is required!');
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+  }
+  
+  return (
+    <View style={styles.container}>
+      <Image source={{url: 'https://brand.colostate.edu/wp-content/uploads/sites/47/2019/01/CSU-Ram-357-617.png'}}
+             style={styles.logo} />
+      <Text style={styles.insts}>
+           Press the button below to select an image on your phone!
+      </Text>
+
+     <TouchableOpacity style={styles.button} onPress={() => alert('You have not selected an image yet')}>
+       <Text style={styles.buttonText}>Pick Image </Text>
+       </TouchableOpacity>     
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: '#FFFFE0'
+  },
+
+  button: {
+    backgroundColor: '#778899',
+    padding: 20,
+    borderRadius: 5
+  },
+
+  insts: {
+    fontSize: 10,
+    color: '#87CEFA',
+    marginHorizontal: 15,
+    marginBottom: 10
+  },
+
+  logo: {
+    width: 305,
+    height: 300,
+    marginBottom: 20
+  },
+
+  buttonText: {
+    fontSize: 20,
+    color: '#ffff'
+  }
+});
